0cebf261a2c1231a1b62a7d9862200c3045a9720
Paweł <prapdm@users.noreply.github.com>
2021-09-16 13:37:51 +0200

    fix: try, catch con.Open()

--- Delegacje Służbowe/ConnectionClass.cs
+++ Delegacje Służbowe/ConnectionClass.cs
@@ -9,7 +9,7 @@ namespace Delegacje_Służbowe
     {
 
         public SqlConnection con;
-        string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=" + $@"{Application.StartupPath}Database.mdf;" + @"Integrated Security=True;Connect Timeout=30";
+        string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=" + $@"{Application.StartupPath}\Database.mdf;" + @"Integrated Security=True;Connect Timeout=30";
 
         public ConnectionClass()
         {
@@ -21,9 +21,32 @@ namespace Delegacje_Służbowe
         }
         private SqlConnection OpenConnection()
         {
-            con = new SqlConnection(connectionString);
-            con.Open();
+             
+            SqlConnection con = new SqlConnection(connectionString);
+
+            try
+            {
+                con.Open();
+                
+            }
+            catch (SqlException ex)
+            {
+                DisplaySqlErrors(ex);
+            }
+
             return con;
+        }
+
+        private static void DisplaySqlErrors(SqlException exception)
+        {
+
+            string errors = "";
+            for (int i = 0; i < exception.Errors.Count; i++)
+            {
+                
+                errors+= "Index #" + i + "\n" + "Error: " + exception.Errors[i].ToString() + "\n";
+            }
+            MessageBox.Show(errors, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
         }
 
 
